0efbfccf0db46c6173c43d82faf6fc0e027fb624
Denis Zhukov <DenisZhukov.Hachiko@gmail.com>
2025-01-03 17:40:51 +0300

    Revert "JWT and OAuth 2.0 auth are added (#5)"
    This reverts commit ff0477be6f484e905b179dda6d5e9b24420a9a8a.

--- packages/authorization/src/main.ts
+++ packages/authorization/src/main.ts
@@ -1,20 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: '*',
-    credentials: true,
-  });
-  app.use(cookieParser());
-  app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
-
   await app.listen(process.env.PORT ?? 3000);
 }
-
 bootstrap();
